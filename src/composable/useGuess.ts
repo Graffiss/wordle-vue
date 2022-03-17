26d7dcf0dbdd645ae65801fff048e9b8c6ec3057
Andrzej Bogdoł <57543124+Graffiss@users.noreply.github.com>
2022-03-17 16:21:14 +0100

    Added working store with usePrevious hook

--- src/composable/useGuess.ts
+++ src/composable/useGuess.ts
@@ -2,8 +2,6 @@ import { ref, onMounted, onUnmounted } from "vue";
 import { WORD_LENGTH } from "../constants/constants";
 
 export const useGuess = () => {
-  // state encapsulated and managed by the composable
-  // const state = reactive({ guess: "" });
   const guess = ref("");
 
   const addGuessLetter = (letter: string) => {
@@ -22,43 +20,17 @@ export const useGuess = () => {
     }
 
     if (newGuess.length === WORD_LENGTH) {
-      return (guess.value = newGuess);
+      guess.value = newGuess;
+      return guess.value;
     }
 
-    // console.log("new guess:", newGuess);
-
-    // guess.value =
-    //   letter.length === 1 && guess.value.length !== WORD_LENGTH
-    //     ? guess.value + letter
-    //     : guess.value;
-
-    // switch (letter) {
-    //   case "Backspace":
-    //     return guess.value.slice(0, -1);
-    //   case "Enter":
-    //     if (guess.value.length === WORD_LENGTH) {
-    //       return "";
-    //     }
-    // }
-
-    // if (guess.value.length === WORD_LENGTH) {
-    //   return guess.value;
-    // }
-
     guess.value = newGuess;
-
-    console.log("new guess:", newGuess);
-    console.log("guess value after addNewLetter:", guess.value);
-
-    return newGuess;
   };
 
   const onKeyDown = (e: KeyboardEvent) => {
     const letter = e.key;
     addGuessLetter(letter);
   };
-
-  console.log("Guess:", guess.value);
 
   // a composable can also hook into its owner component's
   // lifecycle to setup and teardown side effects.
